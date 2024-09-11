a9b869aff5a43cfe6ea6e86a6e356528c79b7bff
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-11 13:00:41 -0500

    put in existing portoflio into this project

--- portfolio_pb_client/vite.config.js
+++ portfolio_pb_client/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: "../pb_public"
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  define: {
+    "process.env": `import.meta.env.VITE_APP_SPACE_TOKEN`,
   }
 })
