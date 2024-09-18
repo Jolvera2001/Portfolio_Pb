f6c72c2b6a27663fd114d7ee814e60bc348bae02
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-18 11:44:04 -0500

    commented out unused theme

--- portfolio_pb_client/src/main.jsx
+++ portfolio_pb_client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      font:  "nunito"
-    }
-  }
-})
+// const theme = extendTheme({
+//   styles: {
+//     global: {
+//       font:  "nunito"
+//     }
+//   }
+// })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
